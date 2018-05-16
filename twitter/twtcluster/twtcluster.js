fcb935bdfc919c935acc67ecf154e3cdd688fd7d
DAN QIN <35333920+QindanUCL@users.noreply.github.com>
2018-05-16 10:54:39 +0100

    Update twtcluster.js

--- twitter/twtcluster/twtcluster.js
+++ twitter/twtcluster/twtcluster.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZGFubmFxaW4iLCJhIjoiY2l3b2RwY2VlMDAyYzJ6b2hsM
 
 //create sync maps
 var wkdayMap = new mapboxgl.Map({
-    container: 'wkday',
+    container: 'wkday',  //div id in html
     style: 'mapbox://styles/dannaqin/cjh4q4he5385p2rpulpxzb1xw',
     center: [-0.1, 51.55],
     zoom: 9,
@@ -12,7 +12,7 @@ var wkdayMap = new mapboxgl.Map({
 });
 
 var wkendMap = new mapboxgl.Map({
-    container: 'wkend',
+    container: 'wkend',  //div id in html
     style: 'mapbox://styles/dannaqin/cjh688cui4miq2rpuhgu45fmi',
     center: [-0.1, 51.55],
     zoom: 9,
@@ -23,13 +23,14 @@ var wkendMap = new mapboxgl.Map({
 syncMaps(wkdayMap, wkendMap);
 
 
-
+//actions after map load
 wkdayMap.on('load', function () {
 
-    //create legend
+    //colors and texts of legend
     var layers = ['0-0.4', '0.4-0.6', '0.6-0.9', '0.9-1.2', '1.2-3', '3-5', '>5'];
     var colors = ['#043371', '#055194', '#0573bd', '#0999dc', '#3eb4f9', '#95dbfe', '#e9f5fb'];
 
+    //create legend
     for (i = 0; i < layers.length; i++) {
         var layer = layers[i];
         var color = colors[i];
@@ -47,7 +48,7 @@ wkdayMap.on('load', function () {
         legend.appendChild(item);
     }
 
-    //add hover border layer
+    //add border layer for hover effect
     wkdayMap.addLayer({
         id: 'wkday-border',
         type: 'line',
@@ -67,6 +68,8 @@ wkdayMap.on('load', function () {
 
 
 wkendMap.on('load', function () {
+    
+    //add border layer for hover effect
     wkendMap.addLayer({
         id: 'wkend-border',
         type: 'line',
@@ -102,10 +105,6 @@ wkdayMap.on('mousemove', function (e) {
     wkdayMap.setFilter("wkday-border", ["==", "name", twt[0].properties.name]);
 });
 
-wkdayMap.on("mouseleave", function () {
-    wkdayMap.setFilter("border", ["==", "name", ""]);
-});
-
 //hover effect-wkend map
 wkendMap.on('mousemove', function (e) {
     var twt = wkendMap.queryRenderedFeatures(e.point, {
@@ -121,4 +120,12 @@ wkendMap.on('mousemove', function (e) {
     
     //hover-show border
     wkendMap.setFilter("wkend-border", ["==", "name", twt[0].properties.name]);
+});
+
+//no border when mouseleave
+wkdayMap.on("mouseleave", "london-wkcount", function () {
+    wkdayMap.setFilter("wkday-border", ["==", "name", ""]);
+});
+wkendMap.on("mouseleave","london-wkendcount", function () {
+    wkendMap.setFilter("wkend-border", ["==", "name", ""]);
 });
